e265628d956de4c1d55b4ec949b9d674b5a40287
Tulesh Ghelani <ghelanitulesh@gmail.com>
2024-12-01 21:46:52 +0530

    Added new enquiry status

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -3,6 +3,9 @@ export const STATUS_OPTIONS = [
   { value: 'P', viewValue: 'Pending' },
   { value: 'W', viewValue: 'Win' },
   { value: 'I', viewValue: 'Interested' },
+  { value: 'B', viewValue: 'Busy another call' },
+  { value: 'N', viewValue: 'Not answering' },
+  { value: 'C', viewValue: 'Check & update' },
   // Add more status options as needed
 ];
 
